d18705dffaf14f493e752d9697af3f90d0fc7607
loverofmusic <1073972000@qq.com>
2020-01-15 21:19:03 +0800

    redux devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,8 @@
 import { createStore } from "redux";
 import reducer from "./reducer";
 
-const store = createStore(reducer);//创建一个管理员的时候要把小本子给他
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : undefined
+); //创建一个管理员的时候要把小本子给他
 export default store;
