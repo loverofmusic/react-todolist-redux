f0340c880560eee4a98534557125bbf4f35fbc4f
loverofmusic <1073972000@qq.com>
2020-01-15 20:26:01 +0800

    redux actiontypes

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "antd/dist/antd.css";
 import { Input, Button, List } from "antd";
 import store from "./store";
+import {CHANGE_INPUT_VALUE, ADD_TODO_ITEM, DELETE_TODO_ITEM} from './store/actionTypes'
 
 export default class Todolist extends Component {
   constructor(props) {
@@ -51,7 +52,7 @@ export default class Todolist extends Component {
 
   handleInputChange(e) {
     const action = {
-      type: "change_input_value",
+      type: CHANGE_INPUT_VALUE,
       value: e.target.value
     };
     store.dispatch(action);
@@ -63,14 +64,14 @@ export default class Todolist extends Component {
 
   handleBtnChange(){
     const action = {
-      type: "add_todo_item"
+      type: ADD_TODO_ITEM
     };
     store.dispatch(action);
   }
 
   handleItemDelete(index){
     const action = {
-      type: "delete_todo_item",
+      type: DELETE_TODO_ITEM,
       index: index
     }
     store.dispatch(action);
